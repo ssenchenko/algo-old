a14658ce8f374bc31927aaf92873987b4c365b49
Slava Senchenko <senchenko_s@icloud.com>
2017-01-21 17:00:35 -0500

    String error output

--- utils/myutil.h
+++ utils/myutil.h
@@ -21,7 +21,7 @@ std::string const scUnexpectedEOF("Unxpected end of file");
 // exception for the wrong integer input
 class WrongIntInput : public std::domain_error {
 public:
-    WrongIntInput() : std::domain_error(scWrongNumber) {};
+    WrongIntInput(std::string s) : std::domain_error(scWrongNumber + " " + s) {};
 };
 
 // check and get input number
