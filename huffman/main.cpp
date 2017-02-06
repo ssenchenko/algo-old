663c8f548f380e1456db53ad813398562ef9e5b4
Slava Senchenko <senchenko_s@icloud.com>
2017-02-06 00:26:31 -0500

    maximum weight independant subset

--- huffman/main.cpp
+++ huffman/main.cpp
@@ -5,6 +5,10 @@ int main() {
 	// read the file
 	std::string file = scPath + scFileName;
 	std::vector< HuffmanTree<long>* > ht_array = read_array(file, true);
+	if (ht_array.empty()) {
+		std::cerr << "Nothing has been loaded" << std::endl;
+		return 1;
+	}
 	HuffmanTree<long>* tree = HuffmanTree<long>::build(ht_array);
 	tree->encode("");
 	std::cout << *tree;
---
+++ mwis/main.cpp
@@ -0,0 +1,28 @@
+#include "mwis.hpp"
+#include <iostream>
+#include <array>
+
+int main() {
+	// read the file
+	std::string file = scPath + scFileName;
+	// file = "/Users/slava_senchenko/Documents/Education/Stanford/Algorithms/src/mwis/short-mwis";
+	std::vector<long> path_graph = read_array(file, true);
+	if (path_graph.empty()) {
+		std::cerr << "Nothing has been loaded" << std::endl;
+		return 1;
+	}
+	
+	std::vector<long> mwis = calculate_mwis(path_graph);
+	std::cout << "done 1" << std::endl;
+	std::vector<int> vertices = reconstruct(mwis, path_graph);
+	std::cout << "done 2" << std::endl;
+
+	std::array<int, 8> vv { {1, 2, 3, 4, 17, 117, 517, 997} };
+	std::string answer = "";
+	for (auto &&e : vv) {
+		answer += std::to_string(vertices.at(e - 1));
+	}
+	std::cout << answer << std::endl;
+
+	return 0;
+}
